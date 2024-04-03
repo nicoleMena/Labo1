f87cf3d27e93b208ae44cab1db1246e39cd338c0
nicoleMena <165852878+nicoleMena@users.noreply.github.com>
2024-04-03 04:15:31 +0200

    Update ejercicio3.sh

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,61 +1,65 @@
 #!/bin/bash
-# Para el menu se implementa:
-print_help() {
-    echo "Uso: $0 [-h] [-m MODE] [-d DATE]"
-    echo "  -h        Imprime menú de ayuda"
-    echo "  -m MODE   Especifique el modo de funcionamiento del informe: servidor_web, base_de_datos, proceso_batch, aplicación, monitoreo"
-    echo "  -d DATE   Especifique la fecha (ejemplo: 2024-03-08)"
+
+# Menú de ayuda:
+imprimir_menu() {
+    echo "Uso: ejercicio3.sh [-h] [-m MODE] [-d DATE]"
+    echo "  -h           Imprimir este menú de ayuda"
+    echo "  -m MODE      Modo de funcionamiento del informe: servidor_web, base_de_datos, proceso_batch, aplicación, monitoreo"
+    echo "  -d DATE      Especifica la fecha en el formato año-mes-día (ejemplo: 2024-03-08)"
 }
 
-# Para generar el reporte, la funcion:
-generate_report() {
-    if [ -n "$mode" ] && [ -n "$date" ]; then
-        grep "ERROR.$mode.$date" log_*.log | while read -r line; do
-            echo "Fecha: $(echo $line | cut -d' ' -f1)"
-            echo "Hora del Error: $(echo $line | cut -d' ' -f2)"
-            echo "Descripción del Error: $(echo $line | cut -d' ' -f5-)"
-        done
-    elif [ -n "$mode" ]; then
-        grep "ERROR.$mode" log_.log | while read -r line; do
-            echo "Fecha: $(echo $line | cut -d' ' -f1)"
-            echo "Hora del Error: $(echo $line | cut -d' ' -f2)"
-            echo "Descripción del Error: $(echo $line | cut -d' ' -f5-)"
-        done
+# Generar el informe
+generar_reporte() {
+    local mode="$1"
+    local date="$2"
+
+    if [ -n "$mode" ]; then
+        if [ -n "$date" ]; then
+            # Informe con modo y fecha especificados
+            grep "ERROR \[$mode\]" log_"$date".log | while read -r line; do
+                echo "Fecha: $date"
+                echo "$line" | cut -d' ' -f2,3,5-
+            done
+        else
+            # Informe solo con modo especificado
+            grep "ERROR \[$mode\]" log_*.log | while read -r line; do
+                echo "$line" | cut -d' ' -f1,2,4,6-
+            done
+        fi
     elif [ -n "$date" ]; then
-        grep "ERROR.$date" log_.log | while read -r line; do
-            echo "Fecha: $(echo $line | cut -d' ' -f1)"
-            echo "Hora del Error: $(echo $line | cut -d' ' -f2)"
-            echo "Descripción del Error: $(echo $line | cut -d' ' -f5-)"
+        # Informe solo con fecha especificada
+        grep "ERROR" log_"$date".log | while read -r line; do
+            echo "$line" | cut -d' ' -f1,2,4,6-
         done
-    else
-        echo "Especificar al menos un modo (-m MODE) o una fecha (-d DATE)."
-        exit 1
     fi
 }
 
+# Procesar opciones con ayuda del comando getopts
 while getopts ":hm:d:" opt; do
     case ${opt} in
-        h)
+        h )
             print_help
             exit 0
             ;;
-        m)
-            mode=$OPTARG
+        m )
+            mode="$OPTARG"
             ;;
-        d)
-            date=$OPTARG
+        d )
+            date="$OPTARG"
             ;;
-        \?)
-            echo "Opción inválida: -$OPTARG" >&2
+        \? )
+            echo "La opción no es valida, intente después: -$OPTARG" >&2
             print_help
             exit 1
             ;;
-        :)
-            echo "La opción -$OPTARG requiere un argumento." >&2
+        : )
+            echo "La opción -$OPTARG necesita un argumento valido." >&2
             print_help
             exit 1
             ;;
     esac
 done
+shift $((OPTIND -1))
 
-generate_report()
+# Llamado a la funcion para generar el informe
+generar_reporte "$mode" "$date"
